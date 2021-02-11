04f57da47f12fd4c4d3650d908d3b7dbb396b4cd
xxki99 <71215906+xxki99@users.noreply.github.com>
2021-02-11 14:18:48 +0800

    Refactor ActorEntity to Actor

--- src/main/java/com/dvdrental/webStore/Repositories/ActorRepository.java
+++ src/main/java/com/dvdrental/webStore/Repositories/ActorRepository.java
@@ -1,9 +1,9 @@
 package com.dvdrental.webStore.Repositories;
 
-import com.dvdrental.webStore.Entities.ActorEntity;
+import com.dvdrental.webStore.Entities.Actor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActorRepository extends JpaRepository<ActorEntity, Long> {
+public interface ActorRepository extends JpaRepository<Actor, Long> {
     
 }
