f24d4291db5170d5ca1986c039eed0adcbc5bc9a
xxki99 <71215906+xxki99@users.noreply.github.com>
2021-02-14 01:09:04 +0800

    Add filmsSubset

--- src/main/java/com/dvdrental/webStore/Entities/Actor.java
+++ src/main/java/com/dvdrental/webStore/Entities/Actor.java
@@ -1,10 +1,17 @@
 package com.dvdrental.webStore.Entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "actor")
@@ -12,19 +19,22 @@ public class Actor {
     @Id
     @GeneratedValue
     @Column(name = "actor_id")
-    private Long actorId;
+    private Integer actorId;
 
-    @Column(name="first_name")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name="last_name")
+    @Column(name = "last_name")
     private String lastName;
 
+    @ManyToMany
+    @JoinTable(name = "film_actor", joinColumns = @JoinColumn(name = "actor_id"), inverseJoinColumns = @JoinColumn(name = "film_id"))
+    List<Film> films;
+
     // id
-    public Long getActorId() {
+    public Integer getActorId() {
         return this.actorId;
     }
-
 
     // first name
     public String getFirstName() {
@@ -35,7 +45,6 @@ public class Actor {
         this.firstName = firstName;
     }
 
-
     // last name
     public String getLastName() {
         return this.lastName;
@@ -45,5 +54,22 @@ public class Actor {
         this.lastName = lastName;
     }
 
+    @JsonIgnore
+    public String[] getFilmsSubsetTitle() {
+        int sizeUpper = 5;
+        boolean isExtra = (films.size() > sizeUpper);
+        int size = isExtra ? sizeUpper + 1 : films.size();
+        String[] returnValue = new String[size];
+        for (int i = 0; i < size; i++) {
+            if (isExtra && i == size - 1) {
+
+                returnValue[i] = "...";
+            } else {
+
+                returnValue[i] = this.films.get(i).getTitle();
+            }
+        }
+        return returnValue;
+    }
 
 }
