ba76477642278cb460e643782ab1fd59f93ed900
xxki99 <71215906+xxki99@users.noreply.github.com>
2021-02-15 16:12:57 +0800

    Use react-router

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,22 +2,47 @@ const React = require("react")
 const ReactDOM = require("react-dom")
 import { useState, useEffect } from "react"
 import ActorsPage from "./actors"
-
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-
     // render
     return (
         <div>
-            
-            <ActorsPage />
+            <NavBar />
+
+                <Router>
+                    <Switch>
+                        <Route path="/">
+                            
+                            <ActorsPage />
+                        </Route>
+                        <Route path="/actors">
+                            
+                            <ActorsPage />
+                        </Route>
+                    </Switch>
+                </Router>
+
         </div>
     )
 }
 
-
-
+function NavBar() {
+    return (
+        <div className="customNavBar navbar-light bg-dark">
+            <ul className="nav justify-content-center">
+                <li className="nav-item">
+                    
+                    <a className="nav-link text-white" href="#">Actor</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById("react"))
